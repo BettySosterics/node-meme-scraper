fdb69a05621b91e63cb49f5340e0ed27ea97eb47
Betty Sosterics <betty.sosterics@gmail.com>
2023-09-08 14:41:34 +0200

    Install axios package for http request

---
+++ index copy.js
@@ -0,0 +1,33 @@
+/* import fs from 'node:fs';
+
+/* const path = './memes';
+
+fs.access(path, (error) => {
+  // To check if the given directory
+  // already exists or not
+  if (error) {
+    // If current directory does not exist
+    // then create it
+    fs.mkdir(path, (error) => {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('New Directory created successfully !!');
+      }
+    });
+  } else {
+    console.log('Given Directory already exists !!');
+  }
+});
+
+const imgList = document.querySelectorAll('img'); */
+
+// fetch request
+function returnImgUrls() {
+  fetch('https://memegen-link-examples-upleveled.netlify.app/')
+    // response to text request
+    .then((response) => response.text())
+    // print data
+    .then((data) => console.log('data from fetch with promises', data))
+    .catch((error) => console.log(error));
+}
