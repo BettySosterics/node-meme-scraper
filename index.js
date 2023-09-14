90d62cf58a0b785eb0333f14eeed4fd50d9be5c6
Betty Sosterics <betty.sosterics@gmail.com>
2023-09-14 17:58:01 +0200

    Fix download code

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import * as fs from 'node:fs';
+import https from 'node:https';
 import path from 'node:path';
 import cheerio from 'cheerio';
-import download from 'download';
+// import download from 'download';
 import fetch from 'node-fetch';
 
 // create MEMES folder
@@ -22,56 +23,24 @@ fs.access(folderPath, (err) => {
   }
 });
 
-async function fetchMemes() {
-  // Access the website
+// Access the website
 
-  const fetchWebsite = await fetch(
-    'https://memegen-link-examples-upleveled.netlify.app/',
-  );
-  const body = await fetchWebsite.text();
+const fetchWebsite = await fetch(
+  'https://memegen-link-examples-upleveled.netlify.app/',
+);
+const body = await fetchWebsite.text();
 
-  const $ = cheerio.load(body);
-  const memeUrls = [];
-  $('img').each((index, element) => {
-    memeUrls.push($(element).attr('src'));
-  });
+const $ = cheerio.load(body);
+const memeUrls = [];
+$('img').each((index, element) => {
+  memeUrls.push($(element).attr('src'));
+});
 
-  const fetchedMemes = memeUrls.slice(0, 10);
+const fetchedMemes = memeUrls.slice(0, 10);
 
-  for (let i = 0; i < fetchedMemes.length; i++) {
-    const imageUrl = fetchedMemes[i];
-    const imageName = `${(i + 1).toString().padStart(2, '0')}.jpg`;
-    const imagePath = path.join(folderPath, imageName);
-
-    if (!(await fileExists(imagePath))) {
-      console.log(`Downloading ${imageName}...`);
-      await download(imageUrl, imagePath);
-    } else {
-      console.log(`${imageName} already exists. Skipping...`);
-    }
-  }
+for (let i = 0; i < fetchedMemes.length; i++) {
+  const imageName = `${(i + 1).toString().padStart(2, '0')}.jpg`;
+  const imagePath = path.join(folderPath, imageName);
+  const imageFile = fs.createWriteStream(imagePath);
+  https.get(fetchedMemes[i].src, (response) => response.pipe(imageFile));
 }
-
-async function fileExists(file) {
-  try {
-    await fs.access(file);
-    return true;
-  } catch (error) {
-    return false;
-  }
-}
-
-(async () => {
-  try {
-    await fetchMemes();
-  } catch (error) {
-    console.error('Error:', error.message);
-  }
-})()
-  .then(() => {
-    process.exit(0);
-  })
-  .catch((error) => {
-    console.error('Unhandled error:', error.message);
-    process.exit(1);
-  });
