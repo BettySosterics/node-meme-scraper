ca82551f62b63e0e1421d0cb3861e6df8d3cb12b
Betty Sosterics <betty.sosterics@gmail.com>
2023-09-14 16:08:00 +0200

    Add image download function

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 import * as fs from 'node:fs';
-// import { argv } from 'node:process';
-// import url from 'node:url';
+import path from 'node:path';
+import cheerio from 'cheerio';
+import download from 'download';
 import fetch from 'node-fetch';
-import { parse } from 'node-html-parser';
 
 // create MEMES folder
 
-const path = './memes';
+const folderPath = './memes';
 
-fs.access(path, (error) => {
+fs.access(folderPath, (error) => {
   if (error) {
-    fs.mkdir(path, (error) => {
+    fs.mkdir(folderPath, (error) => {
       if (error) {
         console.log(error);
       } else {
@@ -22,16 +22,63 @@ fs.access(path, (error) => {
   }
 });
 
-// Access the website
-const fetchWebsite = await fetch(
-  'https://memegen-link-examples-upleveled.netlify.app/',
-);
-const body = await fetchWebsite.text();
+async function fetchMemes() {
+  // Access the website
 
-const images = parse(body).querySelector('#images').querySelectorAll('img');
+  const fetchWebsite = await fetch(
+    'https://memegen-link-examples-upleveled.netlify.app/',
+  );
+  const body = await fetchWebsite.text();
+
+  const $ = cheerio.load(body);
+  const memeUrls = [];
+  $('img').each((index, element) => {
+    memeUrls.push($(element).attr('src'));
+  });
+
+  const first10Memes = memeUrls.slice(0, 10);
+
+  for (let i = 0; i < first10Memes.length; i++) {
+    const imageUrl = first10Memes[i];
+    const imageName = `${(i + 1).toString().padStart(2, '0')}.jpg`;
+    const imagePath = path.join(folderPath, imageName);
+
+    if (!(await fileExists(imagePath))) {
+      console.log(`Downloading ${imageName}...`);
+      await download(imageUrl, imagePath);
+    } else {
+      console.log(`${imageName} already exists. Skipping...`);
+    }
+  }
+}
+
+async function fileExists(file) {
+  try {
+    await fs.access(file);
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
+
+(async () => {
+  try {
+    await fetchMemes();
+  } catch (error) {
+    console.error('Error:', error.message);
+  }
+})()
+  .then(() => {
+    process.exit(0);
+  })
+  .catch((error) => {
+    console.error('Unhandled error:', error.message);
+    process.exit(1);
+  });
 
 // limit the number of images
-images.splice(10);
+
+/* images.splice(10);
 
 let fileName = '';
 
@@ -57,3 +104,4 @@ for (let i = 0; i <= 10; i++) {
       console.error(error);
     });
 }
+*/
